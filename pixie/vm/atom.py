4fb90f615576fede18de668b3b96eea9a29d5d14
egregius313 <egregius313@gmail.com>
2017-06-15 21:20:28 -0400

    Updates to the Atom class
    Added the IWatch#-with-watch and the IValidate#-with-validator methods.

--- pixie/vm/atom.py
+++ pixie/vm/atom.py
@@ -13,17 +13,40 @@ class Atom(object.Object):
     def meta(self):
         return self._meta
 
-    def __init__(self, boxed_value, meta=nil):
+    def with_validator(self, validator):
+        return Atom(self._boxed_value, 
+                    self._meta,
+                    self._watch_key,
+                    self._watch_fn,
+                    validator=validator)
+    
+    def with_watch(self, watch_key, watch_fn):
+        return Atom(self._boxed_value,
+                    self._meta,
+                    watch_key,
+                    watch_fn,
+                    self._validator)
+    
+    def __init__(self, boxed_value, 
+                 meta=nil,
+                 watch_key=nil, watch_fn=nil,
+                 validator=nil):
         self._boxed_value = boxed_value
         self._meta = meta
+        self._watch_key = watch_key
+        self._watch_fn = watch_fn
+        self._validator = validator
 
 
 @extend(proto._reset_BANG_, Atom)
 def _reset(self, v):
     assert isinstance(self, Atom)
+    if self._validator is not nil:
+        affirm(self._validator.invoke([v]), u"Invalid State Exception: Invalid reference state.")
+    if self._watch_fn is not nil:
+        self._watch_fn.invoke([self._watch_key, self, self._boxed_value, v])
     self._boxed_value = v
     return v
-
 
 @extend(proto._deref, Atom)
 def _deref(self):
@@ -39,6 +62,16 @@ def _meta(self):
 def _with_meta(self, meta):
     assert isinstance(self, Atom)
     return self.with_meta(meta)
+
+@extend(proto._with_watch, Atom)
+def _with_watch(self, watch_key, watch_fn):
+    assert isinstance(self, Atom)
+    return self.with_watch(watch_key, watch_fn)
+
+@extend(proto._with_validator, Atom)
+def _with_validator(self, validate_fn):
+    assert isinstance(self, Atom)
+    return self.with_validator(validate_fn)
 
 @as_var("atom")
 def atom(val=nil):
